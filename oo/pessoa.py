e63ee3947da7e380c213110135ffa8f3086841b7
alexv <alex.victor@outlook.com>
2020-04-01 11:55:50 -0300

    Criado método cumprimentar

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,3 +1,11 @@
 #criar uma classe.
 class Pessoa:   #o nome sempre com a 1 letra maiuscula, ex: ExemploPessoa.
-    pass
+    def cumprimentar(self): #metodo é uma funcão da classe
+        return f'olá {id(self)}'
+
+if __name__ == '__main__':
+    p = Pessoa()
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar()) # todos os 3 metodos do print imprimo o mesmo metodo
+
