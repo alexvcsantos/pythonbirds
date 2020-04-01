b6079032f9a5ce5a9172ab994c80f63002596c14
alexv <alex.victor@outlook.com>
2020-04-01 13:13:49 -0300

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,18 +1,21 @@
 #criar uma classe.
 class Pessoa:   #o nome sempre com a 1 letra maiuscula, ex: ExemploPessoa.
-    def __init__(self, nome=None, idade=35):  #para criar atributos de um metodo, é usado um metodo especial, jaá pode criar um atributo e definir o valor nulo.
+    # para criar atributos de um metodo, é usado um metodo especial, jaá pode criar um atributo e definir o valor nulo.
+    def __init__(self, *filhos, nome=None, idade=35):  #*filhos atributo variavel
         self.idade = idade
         self.nome = nome   #para criar um atributo de um objeto utiliza self.atributo = None ou seja nulo
+        self.filhos = list(filhos)
     def cumprimentar(self): #metodo é uma funcão da classe
         return f'olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Luciano')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar()) # todos os 3 metodos do print imprimo o mesmo metodo
-    print(p.nome)
-    p.nome = 'Alex' #alterar o conteudo do atributo
-    print(p.nome)
-    print(p.idade)
+    alex = Pessoa(nome='Alex')
+    luciano = Pessoa(alex, nome='Luciano')
+    print(Pessoa.cumprimentar(luciano))
+    print(id(luciano))
+    print(luciano.cumprimentar()) # todos os 3 metodos do print imprimo o mesmo metodo
+    print(luciano.nome)
+    print(luciano.idade)
+    for filho in luciano.filhos:
+        print(filho.nome)
 
